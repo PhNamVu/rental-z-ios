2ff24cfe7e3d2b3bfcf022e77cfea7eeda23f9d9
Nam Vu <phnamvu@gmail.com>
2021-09-07 21:12:54 +0700

    Initial Commit

---
+++ RentalApartments/RentalApartmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalApartmentsApp.swift
+//  RentalApartments
+//
+//  Created by Nam Vu on 07/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RentalApartmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
